3479e53ad907af6798e9e2ed19da7d4a9d789502
sm6142 <smetters@rayquaza.local>
2015-03-25 00:18:11 -0700

    Added extra images, expanded instructions.

--- SpaceMaze/GameScene.swift
+++ SpaceMaze/GameScene.swift
@@ -8,38 +8,86 @@
 
 import SpriteKit
 
+// Enumeration -- defines a variable class with five different values
+enum TouchCommand {
+    case MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    NO_COMMAND
+}
 class GameScene: SKScene {
+    /* A useful constant we'll use later */
+    let PI = CGFloat(M_PI)
+    
+    /* Properties */
+    let character = SKSpriteNode(imageNamed: "Spaceship")  // use Spaceship.png file for the image of the sprite
+    let color = UIColor(red:0.15, green:0.15, blue:0.3, alpha:1.0)
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        self.backgroundColor = color
+        character.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+        character.setScale(0.10)
+        self.addChild(character)   // Make sprite visible
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+        for touch in touches {
+            let command: TouchCommand = commandForTouch(touch as UITouch, node:self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            var move_x:CGFloat = 0
+            var move_y:CGFloat = 0
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            // Here is where you need to insert you code to set how much
+            // to move in the x direction (left / right) or the y direction (up / down)
+            if (command == TouchCommand.MOVE_UP) {
+              //  move_y = 30
+            }
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            if (move_x != 0 || move_y != 0) {
+                let action:SKAction = SKAction.moveByX(move_x, y: move_y, duration: 0.25)
+                self.character.runAction(action)
+            }
         }
+        println("Position: \(self.character.position)")
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    // Figures out which way the user wants to move the character based on which
+    // edge of the screen the user touched.
+    func commandForTouch(touch:UITouch, node:SKNode) -> TouchCommand {
+        let location:CGPoint = touch.locationInNode(node)
+        let frame:CGRect = node.frame
+        let height = CGRectGetHeight(frame)
+        let width = CGRectGetWidth(frame)
+        
+        if (location.y/height < 0.25) {
+            return TouchCommand.MOVE_DOWN
+        }
+        if (location.y/height > 0.75) {
+            return TouchCommand.MOVE_UP
+        }
+        if (location.x/width < 0.25) {
+            return TouchCommand.MOVE_LEFT
+        }
+        if (location.x/width > 0.75) {
+            return TouchCommand.MOVE_RIGHT
+        }
+        return TouchCommand.NO_COMMAND
     }
+
+    /*
+     
+        Hey! I finished all that. What do I do now?
+
+        1. Change your spaceship to a PacMan or...
+        2. Change the color of your background.
+        3. Make your spaceship rotate when you move it. A hint:
+                self.character.zRotation = PI * 0.5
+        4. Make your game "wrap" -- when the spaceship goes off the right side of the screen,
+            have it reappear on the left. A hint:
+                location.x = location.x % width
+    
+    */
 }
