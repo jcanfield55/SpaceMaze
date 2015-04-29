9f684d38d69ce6cd9fcb35d9026cbdbf78ce5b56
John Canfield <john@Johns-MacBook-Pro.local>
2015-04-28 21:59:38 -0700

    Added more intelligent opponent searching:   Now has two modes: moves toward you in one mode    If that mode gets blocked, then goes into random movement mode until it enters a new tunnel

--- SpaceMaze/OpponentCharacter.swift
+++ SpaceMaze/OpponentCharacter.swift
@@ -11,8 +11,11 @@ import SpriteKit
 
 class OpponentCharacter:Character {
     
+    var blockedMoveRandomly:Bool = false
+    
     // Function for opponents to chase you
-    // Can you make it more sophisticated?
+    // Now has two modes: moves toward you in one mode
+    // If that mode gets blocked, then goes into random movement mode until it enters a new tunnel
     
     func chaseCharacter(targetCharacter:Character) {
         let selfPoint:CGPoint = self.currentTunnel.pointAtTunnelPosition(self.tunnelPosition)
@@ -23,22 +26,57 @@ class OpponentCharacter:Character {
         let xCommand:TouchCommand = (xDiff > 0 ? TouchCommand.MOVE_RIGHT : (xDiff == 0 ? TouchCommand.NO_COMMAND :TouchCommand.MOVE_LEFT))
         let yCommand:TouchCommand = (yDiff > 0 ? TouchCommand.MOVE_UP : (yDiff == 0 ? TouchCommand.NO_COMMAND : TouchCommand.MOVE_DOWN))
         
-        if fabs(Double(xDiff)) > fabs(Double(yDiff)) {
-            // Bigger xDiff so try moving horizontally first
-            let success:Bool = self.moveCharacter(xCommand)
-            if !success {
-                self.moveCharacter(yCommand)  // If not successful moving horizontally, try moving vertically
+        if !blockedMoveRandomly {  // Normal movement if not blocked
+            if (xDiff == 0 && yDiff == 0) {
+                return    // No need to move if already on top of targetCharacter
+            }
+            else if fabs(Double(xDiff)) > fabs(Double(yDiff)) {
+                // Bigger xDiff so try moving horizontally first
+                var success:Bool = self.moveCharacter(xCommand)
+                if !success {
+                    success = self.moveCharacter(yCommand)  // If not successful moving horizontally, try moving vertically
+                }
+                if !success {
+                    blockedMoveRandomly = true  // Cannot move either direction, so we are blocked
+                    println("blockedMoveRandomly = true")
+                }
+            }
+            else {
+                // Bigger yDiff so try moving vertically first
+                var success:Bool = self.moveCharacter(yCommand)
+                if !success {
+                    success = self.moveCharacter(xCommand)  // If not succesfull moving vertically, try moving horizontally
+                }
+                if !success {
+                    blockedMoveRandomly = true  // Cannot move either direction, so we are blocked
+                    println("blockedMoveRandomly = true")
+                }
             }
         }
-        else {
-            // Bigger yDiff so try moving vertically first
-            let success:Bool = self.moveCharacter(yCommand)
-            if !success {
-                self.moveCharacter(xCommand)  // If not succesfull moving vertically, try moving horizontally
+        else {  // blockedMoveRandomly = true, do a random movement until into another tunnel
+            let myTunnel:Tunnel = self.currentTunnel
+            var r:UInt32 = arc4random_uniform(4)
+            var direction:TouchCommand = TouchCommand.NO_COMMAND
+            if r == 0 {
+                direction = TouchCommand.MOVE_DOWN
+            } else if r == 1 {
+                direction = TouchCommand.MOVE_LEFT
+            } else if r == 2 {
+                direction = TouchCommand.MOVE_RIGHT
+            } else if r == 3 {
+                direction = TouchCommand.MOVE_UP
             }
-            
+            var success:Bool = self.moveCharacter(direction)
+            println("Move \(r)")
+            if (!success) {
+                self.chaseCharacter(targetCharacter)  // if not successful, recursively try again to get another random direction that allows movement
+                return
+            }
+            if (myTunnel !== self.currentTunnel) {
+                blockedMoveRandomly = false  // We are now unblocked because we have moved into a new tunnel
+                println("blockedMoveRandomly = false")
+            }
         }
     }
     
-    // What if the opponent catches you?  What happens then??    
 }
