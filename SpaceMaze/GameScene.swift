e204839865c635a238d3ea9dcfe99440af42b8ae
Gamer <gamer@johns-macbook-pro.local>
2016-10-26 00:11:17 -0700

    Test commit (comments only)

--- SpaceMaze/GameScene.swift
+++ SpaceMaze/GameScene.swift
@@ -3,7 +3,7 @@
 //  SpaceMaze
 //
 //  Created by Diana Smetters on 3/24/15.
-//  Copyright (c) 2015 Diana Smetters. All rights reserved.
+//  Copyright (c) 2015 Diana Smetters. All rights reserved.  Test
 //
 
 import SpriteKit
