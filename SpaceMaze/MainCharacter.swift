7d5ea1494574b7666d49d193834da4543d00c5cf
John Canfield <john@johns-macbook-pro.local>
2016-11-15 21:36:39 -0800

    Lesson 4 prep

--- SpaceMaze/MainCharacter.swift
+++ SpaceMaze/MainCharacter.swift
@@ -12,5 +12,18 @@ import SpriteKit
 class MainCharacter:Character {
 
     var treasureScore:Int = 0
-
+    var powerUpTimer:NSTimer?
+    //TODO - create a variable to keep track of whether or not you are powered up.
+    
+    
+    // function to call when you catch a PowerTreasure
+    func powerMeUp() {
+        //TODO - set the powered up variable to true
+        powerUpTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target:self, selector:#selector(MainCharacter.powerMeDown(_:)), userInfo: nil, repeats: false)
+    }
+    
+    // function called when the time is up on the PowerTreasure power
+    func powerMeDown(timer: NSTimer) {
+        //TODO - set the powered up variable to false
+    }
 }
