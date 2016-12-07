a5022b645e7d085e9bec337fc44b6780a614fbd5
XCodeLearner <xcodelearner@scclc-l017.local>
2016-12-07 09:22:25 -0800

    Swift 3 updates

--- SpaceMaze/MainCharacter.swift
+++ SpaceMaze/MainCharacter.swift
@@ -12,7 +12,7 @@ import SpriteKit
 class MainCharacter:Character {
 
     var treasureScore:Int = 0
-    var powerUpTimer:NSTimer?
+    var powerUpTimer:Timer?
     //TODO - create a variable to keep track of whether or not you are powered up.
     var powerupmode:Bool = false
     var myLastTexture: SKTexture?
@@ -24,11 +24,11 @@ class MainCharacter:Character {
         powerupmode=true
         myLastTexture = self.texture
         self.texture = SKTexture(imageNamed:"shark")
-        powerUpTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target:self, selector:#selector(MainCharacter.powerMeDown(_:)), userInfo: nil, repeats: false)
+        powerUpTimer = Timer.scheduledTimer(timeInterval: 15.0, target:self, selector:#selector(MainCharacter.powerMeDown(_:)), userInfo: nil, repeats: false)
     }
     
     // function called when the time is up on the PowerTreasure power
-    func powerMeDown(timer: NSTimer) {
+    func powerMeDown(_ timer: Timer) {
         self.texture = myLastTexture
         //TODO - set the powered up variable to false
         powerupmode=false
